8fb1fb89477b15d6587d476fa081bff26bfd7e9e
pythagoras58 <solomondanso58@gmail.com>
2023-12-11 18:05:18 +0800

    BBO main class - constructor declaration

--- src/BBO/BBO.java
+++ src/BBO/BBO.java
@@ -9,4 +9,11 @@ public class BBO {
     BiogeographySolution[] solutions;
     double[] migrationRates;
     int eliteCount;
+
+    BBO(int populationSize, int maxGenerations, double migrationRate, int dimension){
+        this.populationSize = populationSize;
+        this.maxGenerations = maxGenerations;
+        this.migrationRate = migrationRate;
+        this.dimension = dimension;
+    }
 }
