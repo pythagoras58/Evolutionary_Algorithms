90e7564b2ffee3f8a15ce44da9446d5f86ac7018
pythagoras58 <solomondanso58@gmail.com>
2023-12-11 18:24:24 +0800

    BBO test runner

--- src/BBO/BBO.java
+++ src/BBO/BBO.java
@@ -1,5 +1,8 @@
 package BBO;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class BBO {
     int populationSize;
     int maxGenerations;
@@ -25,5 +28,40 @@ public class BBO {
             solutions[i] = new BiogeographySolution(dimension);
             migrationRates[i] = this.migrationRate;
         }
+    }
+
+    void updateMigrationRates() {
+        Arrays.sort(solutions, (a, b) -> Double.compare(a.fitness, b.fitness));
+
+        for (int i = 0; i < populationSize; i++) {
+            migrationRates[i] = migrationRate * (1 - (double) i / populationSize);
+        }
+    }
+
+    void migrateSolutions() {
+        Random rand = new Random();
+        for (int i = eliteCount; i < populationSize; i++) {
+            if (rand.nextDouble() < migrationRates[i]) {
+                int sourceIndex = rand.nextInt(eliteCount);
+                System.arraycopy(solutions[sourceIndex].habitat, 0, solutions[i].habitat, 0, dimension);
+                solutions[i].fitness = solutions[i].evaluateFitness();
+            }
+        }
+    }
+
+    void optimize() {
+        for (int generation = 0; generation < maxGenerations; generation++) {
+            updateMigrationRates();
+            migrateSolutions();
+
+            // Optionally, you can print the best fitness at each generation
+            Arrays.sort(solutions, (a, b) -> Double.compare(a.fitness, b.fitness));
+            System.out.println("Generation " + (generation + 1) + ": Best Fitness = " + solutions[0].fitness);
+        }
+
+        // Print the final result
+        Arrays.sort(solutions, (a, b) -> Double.compare(a.fitness, b.fitness));
+        System.out.println("Optimization completed. Best fitness: " + solutions[0].fitness);
+        System.out.print("Best habitat: " + Arrays.toString(solutions[0].habitat));
     }
 }
---
+++ src/BBO/BBOTestRunner.java
@@ -0,0 +1,7 @@
+package BBO;
+
+public class BBOTestRunner {
+    public static void main(String[] args) {
+
+    }
+}
