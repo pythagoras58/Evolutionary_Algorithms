16737e85147326f0c4d01340b0134fb7e9a88211
pythagoras58 <solomondanso58@gmail.com>
2023-12-11 17:27:37 +0800

    BBO evaluation function

--- src/BBO/BiogeographySolution.java
+++ src/BBO/BiogeographySolution.java
@@ -1,10 +1,27 @@
 package BBO;
 
+import java.util.Random;
+
 public class BiogeographySolution {
     double[] habitat;
     double fitness;
 
     BiogeographySolution(int dimension){
+        habitat = new double[dimension];
 
+        Random rand = new Random();
+
+        for(int i=0; i<dimension; i++){
+            habitat[i] = rand.nextDouble(); //Initialize habitat randomly between 0 and 1
+
+        }
+
+        // Evaluate fitness based on the objective function
+        fitness = evaluateFitness();
+    }
+
+    double evaluateFitness() {
+
+        return 0.0;
     }
 }
