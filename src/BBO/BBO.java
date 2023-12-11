16698dcbccf290e002d449e6c96fb9302f26ab59
pythagoras58 <solomondanso58@gmail.com>
2023-12-11 17:56:06 +0800

    BBO main class

---
+++ src/BBO/BBO.java
@@ -0,0 +1,12 @@
+package BBO;
+
+public class BBO {
+    int populationSize;
+    int maxGenerations;
+    double migrationRate;
+    int dimension;
+
+    BiogeographySolution[] solutions;
+    double[] migrationRates;
+    int eliteCount;
+}
